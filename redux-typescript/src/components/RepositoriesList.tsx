d74941cb0aa496dceac193a284f3cba476314abd
thijsBoet <m.boet@chello.nl>
2022-08-07 16:09:54 +0200

    finished redux example

--- redux-typescript/src/components/RepositoriesList.tsx
+++ redux-typescript/src/components/RepositoriesList.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { actionCreators } from '../state';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
 	const [term, setTerm] = useState('');
-	const dispatch = useDispatch();
+	const { searchRepositories } = useActions();
+	const { data, error, loading } = useTypedSelector(
+		state => state.repositories
+	);
 
 	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
-		dispatch(actionCreators.searchRepositories(term) as any);
+		searchRepositories(term);
 	};
 
 	return (
@@ -22,6 +25,9 @@ const RepositoriesList: React.FC = () => {
 				/>
 				<button type='submit'>Search</button>
 			</form>
+			{error && <h3>{error}</h3>}
+			{loading && <h3>Loading...</h3>}
+			{!error && !loading && data.map(name => <div key={name}>{name}</div>)}
 		</div>
 	);
 };
